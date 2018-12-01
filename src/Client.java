c4636ccbbe5a2f25904d9be4774c77c695fa926f
Unknown <ciuffi9@mac.com>
2018-12-01 15:03:49 -0800

    add dict reader with decoder to client

--- src/Client.java
+++ src/Client.java
@@ -13,10 +13,8 @@ import java.util.Map;
  */
 
 class updateHandler implements Runnable {
-    public void run(){
-        while (true){
-            Client.updateDict();
-        }
+    public void run() {
+        Client.updateDict();
     }
 }
 
@@ -24,24 +22,23 @@ public class Client {
 
     private static Socket kkSocket;
     private static PrintWriter out;
+    private static updateHandler uHandler;
     private static BufferedReader in;
     private static boolean connected = false;
     private static Map<String, Double[]> IPDict = new HashMap<>();
-    public static void main(String[] main){
-        JoinSession("127.0.0.1", 3000);
+
+    private Client(String ip, int port) {
+        JoinSession(ip, port);
+        uHandler = new updateHandler();
+        uHandler.run();
     }
 
 
-    public Client(){
-
-    }
-
-
-    public static void JoinSession(String ip, int port){
+    private static void JoinSession(String ip, int port) {
         try {
             kkSocket = new Socket(ip, port);
             out = new PrintWriter(kkSocket.getOutputStream(), true);
-            in = new BufferedReader( new InputStreamReader(kkSocket.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
             System.out.println("Client sending test...");
             out.println("client test");
             connected = true;
@@ -50,43 +47,37 @@ public class Client {
         }
     }
 
-    public static void updateDict(){
-        try{
-            String recieved = in.readLine();
-            if (recieved != null){
-                int index;
-                String newLon;
-                String newLat;
-                index = recieved.indexOf('/');
-                newLat = recieved.substring(0, index);
-                newLon = recieved.substring(index+1, -1);
-                Double[] latlon = {
-                        Double.parseDouble(recieved.substring(0, index)),
-                        Double.parseDouble(recieved.substring(index+1, -1))};
-                IPDict.put(latlon);
-
+    static void updateDict() {
+        while (true) {
+            try {
+                String recieved = in.readLine();
+                if (recieved != null) {
+                    IPDict = dictDecoder.decodeDict(recieved);
+                }
+            } catch (java.io.IOException ex) {
             }
-        }catch (java.io.IOException ex){
-
         }
+    }
 
+    public static Double[] getLatLonById(String id) {
+        return IPDict.get(id);
     }
 
 
-    public String GetHostIp(String code){
+    public String GetHostIp(String code) {
         String[] parts = code.split("z");
 
         String ipAddress = "";
 
-        for(String part : parts){
-            for (int i = 0; i < part.length(); i++){
+        for (String part : parts) {
+            for (int i = 0; i < part.length(); i++) {
                 int c = part.charAt(i);
                 ipAddress = ipAddress + (char) (c - 49);
             }
             ipAddress = ipAddress + ".";
         }
 
-        System.out.println(ipAddress.substring(0,ipAddress.length() - 1));
+        System.out.println(ipAddress.substring(0, ipAddress.length() - 1));
         return ipAddress;
     }
 }
